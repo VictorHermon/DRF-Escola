66cafc55adc82c546b498ed36b2cc3e089eec63c
victorhermon <victorsilva.silva6@gmail.com>
2022-04-24 22:57:27 -0300

    Melhorado o codigo e adicionando no requirements.txt

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,7 @@
 import http
 from rest_framework import viewsets, generics, status
 from escola.models import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, \
-    ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from escola.serializer import *
 from rest_framework.response import Response
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
