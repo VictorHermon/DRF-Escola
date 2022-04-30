db88b73e8926679d33ed44e598610d9a950cadec
victorhermon <victorsilva.silva6@gmail.com>
2022-04-30 11:27:45 -0300

    Adicionado testes da request delete e put

--- test/tests_cursos.py
+++ test/tests_cursos.py
@@ -32,3 +32,18 @@ class CursoTestCase(APITestCase):
         }
         response = self.client.post(self.list_url, data=data)
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    def test_requisicao_delete_para_deletar_curso(self):
+        """Teste para verofocar a requisição delete não permitida para deletar um curso"""
+        response = self.client.delete('/cursos/1/')
+        self.assertEquals(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_requisicao_put_para_atualizar_curso(self):
+        """Teste para verificar requisicao PUT para atualizar um curso"""
+        data = {
+            'codigo_curso': 'CTT1',
+            'descricao': 'Curso de Teste 01 atualizado',
+            'nivel': 'I'
+        }
+        response = self.client.put('/cursos/1/', data=data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
